8498fd345b4628eccaeaa4fc5b207b1b6212269b
Ramuel Gonzalez <ramuelcl@gmail.com>
2022-01-09 16:29:56 +0100

    main

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  {{-- favicon --}}
+  {{-- estilos --}}
 </head>
 
 <body>
+  {{-- header --}}
+  {{-- nav --}}
   bienvenido a ala página principal !
+  {{-- footer --}}
+  {{-- script --}}
 </body>
 
 </html>
